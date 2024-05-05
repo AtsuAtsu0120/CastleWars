f1ed889213ca42b2ea4834b0398955b8dd302966
AtsuAtsu <58623243+AtsuAtsu0120@users.noreply.github.com>
2024-05-05 18:21:18 +0900

    feat: Prefabベースのコードに変更

---
+++ Assets/Scripts/Bakers/Trooper.cs
@@ -0,0 +1,18 @@
+﻿using ComponentData;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Bakers
+{
+    public class Trooper : MonoBehaviour
+    {
+        public class TrooperBaker : Baker<Trooper>
+        {
+            public override void Bake(Trooper authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent<TrooperData>(entity);
+            }
+        }
+    }
+}
